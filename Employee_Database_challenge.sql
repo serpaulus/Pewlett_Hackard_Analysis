a93a25568d486773e4f0475904f5d11b73a13f54
SERGIO PARAMO <sergio.paramo@outlook.com>
2021-08-21 18:27:02 -0500

    module 7 challenge

--- Employee_Database_challenge.sql
+++ Employee_Database_challenge.sql
@@ -28,6 +28,25 @@ FROM unique_titles AS ut
 GROUP BY ut.title
 ORDER BY ut.count DESC;
 
+-- Create a mentorship-eligibility table that holds the current employees who were born between January 1, 1965 and December 31, 1965.
+
+SELECT DISTINCT ON (emp_no) e.emp_no,
+	e.first_name,
+	e.last_name,
+	e.birth_date,
+	de.from_date,
+	de.to_date,
+	ti.title
+INTO mentorship_eligibility
+FROM employees AS e
+	INNER JOIN dept_emp AS de
+		ON (e.emp_no = de.emp_no)
+	INNER JOIN titles AS ti
+		ON (e.emp_no = ti.emp_no)
+WHERE (de.to_date  = '9999-01-01')
+	   AND (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY e.emp_no;
+
 
 
 
